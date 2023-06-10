adc905d3b1358a5bc28595e0cb7a88b10fe64fe4
markortleb <markortleb@gmail.com>
2023-06-10 11:25:55 -0600

    added description

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,17 @@ const Home = () => {
   return (
       <div className='Home'>
           <TopBar />
-          <a href="./cart">Cart</a>
+          <div className="Content">
+              <div className="DescriptionArea">
+                  <span className="DescriptionHeader">Welcome!</span>
+                  <span className="Description">
+                  Here at Mark's Antique Shop, we pride ourselves in providing our customers with relics from the past.
+                  Browse our wares and be transported back to the early 20th Century.
+                  Enhance your living space with an item that will remind us all where we came from.
+                  Become an antique collector today!
+              </span>
+              </div>
+          </div>
       </div>
   );
 };
