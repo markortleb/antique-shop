3a668e5d096372218ec6602e69ef45cf874adb5b
markortleb <markortleb@gmail.com>
2023-06-11 03:38:06 -0600

    make function to load items

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,31 @@
 import '../../styles/app/Home.css';
+import getItemsForSale from "../../util/getItemsForSale";
 import TopBar from "../common/TopBar";
 
 const Home = () => {
-  return (
+
+    const itemsForSale = getItemsForSale();
+
+    return (
       <div className='Home'>
           <TopBar />
           <div className="Content">
               <div className="DescriptionArea">
                   <span className="DescriptionHeader">Welcome!</span>
                   <span className="Description">
-                  Here at Mark's Antique Shop, we pride ourselves in providing our customers with relics from the past.
-                  Browse our wares and be transported back to the early 20th Century.
-                  Enhance your living space with an item that will remind us all where we came from.
-                  Become an antique collector today!
-              </span>
+                      Here at Mark's Antique Shop, we pride ourselves in providing our customers with relics from the past.
+                      Browse our wares and be transported back to the early 20th Century.
+                      Enhance your living space with an item that will remind us all where we came from.
+                      Become an antique collector today!
+                  </span>
               </div>
+              <ul className="ItemsForSale">
+
+
+              </ul>
           </div>
       </div>
-  );
+    );
 };
 
 export default Home;
---
+++ src/util/getItemsForSale.js
@@ -0,0 +1,41 @@
+import uniqid from 'uniqid';
+
+
+export default function getItemsForSale () {
+    function _importAll(r) {
+        return r.keys().map(r);
+    }
+
+    const itemList = [
+        {
+            name: 'Grandfather Clock',
+            description: 'This is a grandfather clock, manufactured in 1855.',
+            price: 30279,
+            itemKey: 'grandfather_clock'
+        },
+        {
+            name: 'Victrola Cabinet',
+            description: 'A well maintained Victor Victrola Phonograph Cabinet. Manufactured in 1920.',
+            price: 1279,
+            itemKey: 'victrola_cabinet'
+        },
+        {
+            name: 'Water Pitcher',
+            description: 'This is a very old water pitcher. Manufactured in 1795.',
+            price: 10000,
+            itemKey: 'water_pitcher'
+        }
+    ];
+
+    const images = _importAll(require.context('../img', false, /\.(png|jpe?g|svg|webp|jpg)$/));
+
+    itemList.forEach(item => {
+        images.forEach(image => {
+            if (image.includes(item.itemKey)) {
+                item['image'] = image;
+            }
+        });
+    });
+
+    return itemList;
+}
